8a2397fbd9ed4991e07144ef358dcd39e8b43284
ZenT3600 <matteo.leggio@tiscali.it>
2023-07-23 11:12:09 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 ls -a dotfiles | xargs -I {} sh -c "ln -f -s $PWD/dotfiles/{} ~/{} &> /dev/null && echo \"Linked {}\" || echo \"Skipping {}\""
+find dotfiles -maxdepth 1 -mindepth 1 -type d | xargs -I {} basename {} | xargs -I {} sh -c "mv -f ~/{} ~/.config/ &> /dev/null && echo \"Moved config folder {} to correct location\" || echo \"Failed moving {}\""
 ln -f -s $PWD/scripts/ -T $HOME/.scripts &> /dev/null && echo "Linked Scripts Folder" || echo "Skipping Scripts Folder"
